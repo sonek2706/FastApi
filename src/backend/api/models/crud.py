f610cd14713972272c05b600adb6671a72f5054a
sonek2706 <s26092@pjwstk.edu.pl>
2023-05-23 12:29:09 +0200

    Small Changes

--- src/backend/api/models/crud.py
+++ src/backend/api/models/crud.py
@@ -8,29 +8,8 @@ def get_Product(db: Session, product_id: int):
 def get_Customer(db: Session, customer_id: str):
     return db.query(models.Customer).filter(models.Customer.customer_id == customer_id).first()
 
-
 def get_Products(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Product).offset(skip).limit(limit).all()
 
 def get_Customers(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Customer).offset(skip).limit(limit).all()
-
-# def create_user(db: Session, user: schemas.UserCreate):
-#     fake_hashed_password = user.password + "notreallyhashed"
-#     db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
-#     db.add(db_user)
-#     db.commit()
-#     db.refresh(db_user)
-#     return db_user
-
-
-# def get_items(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.Item).offset(skip).limit(limit).all()
-
-
-# def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-#     db_item = models.Item(**item.dict(), owner_id=user_id)
-#     db.add(db_item)
-#     db.commit()
-#     db.refresh(db_item)
-#     return db_item
